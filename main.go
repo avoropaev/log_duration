9be3e2e6d97485acdc5c8416dd20acefe75d4d6b
VoropaevAE <aleksejj.voropaev@vseinstrumenti.ru>
2022-05-31 11:46:22 +0600

    add AppMiddleware

--- main.go
+++ main.go
@@ -4,33 +4,18 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"time"
 
 	"github.com/avoropaev/log_duration/external"
 )
 
-// мейн это типо мидлварь сервера
 func main() {
-	app := NewApp(
-		external.NewClientDurationMiddleware(external.NewService1Client()),
+	app := NewAppMiddleware(
+		NewApp(
+			external.NewClientDurationMiddleware(external.NewService1Client()),
+		),
 	)
 
 	ctx := context.Background()
-
-	start := time.Now()
-	defer func() {
-		duration := time.Since(start)
-
-		service1Duration, ok := ctx.Value(external.Service1QueryDurationContextKey).(time.Time)
-		if ok && service1Duration.Second() > 1 {
-			// если был запрос во внешний сервис, который длился больше 1 секунды, то "слишком долго" не логируем
-			return
-		}
-
-		if duration.Seconds() > 2 {
-			log.Print("слишком долго")
-		}
-	}()
 
 	result, err := app.Do(ctx, 123)
 	if err != nil {
